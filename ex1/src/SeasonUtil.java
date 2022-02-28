6dd2dccf402c16191dcc7dcb86191efd555cd788
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2022-02-28 17:58:59 +0200

    implemented sort method

--- ex1/src/SeasonUtil.java
+++ ex1/src/SeasonUtil.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 
 public class SeasonUtil {
     public static final Season[] SEASONS = Season.values();
+
     public static void sortSeasonable(Comparable[] seasonables) {
-        // TODO: Implement.
+        Arrays.sort(seasonables);
     }
 
     static String reportAll(Seasonable[] seasonables) {
-        // TODO: Implement.
+
+
         return null;
     }
 }
