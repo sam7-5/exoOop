0f4bbd3f2467404003e3746d99aa6d21f633e4fe
alexandre lejdstrom <yehouda.lejdstrom@gmail.com>
2022-02-28 17:55:40 +0200

    implemented Olive Tree class

--- ex1/src/SeasonUtil.java
+++ ex1/src/SeasonUtil.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 public class SeasonUtil {
     public static final Season[] SEASONS = Season.values();
     public static void sortSeasonable(Comparable[] seasonables) {
-
-
         // TODO: Implement.
     }
 
