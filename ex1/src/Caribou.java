4ff62a300390458acc53a55c0654df1447e964b9
samuel verse <verse@g.jct.ac.il>
2022-02-28 17:23:25 +0200

    caribou done

--- ex1/src/Caribou.java
+++ ex1/src/Caribou.java
@@ -1,18 +1,39 @@
 public class Caribou extends Animal {
+    private Direction migratingDirection;
 
     Caribou(int weight, Season season) {
-        // TODO: Implement.
-        super(0, null, null);
+        super(weight, season, Color.BROWN);//most seasons the animal is brown
     }
 
     @Override
     public String toString() {
-        // TODO: Implement.
-        return null;
+        if (getCurrentSeason() == Season.WINTER)
+            return "Caribou: I am migrating south. " + super.toString();
+        else if (getCurrentSeason() == Season.SUMMER)
+            return "Caribou: I am migrating north. " + super.toString();
+        else
+            return "Caribou: " + super.toString();
+
     }
 
     @Override
     public void changeSeason() {
-        // TODO: Implement.
+        super.changeSeason();
+
+        switch (getCurrentSeason()) {
+
+            case WINTER:
+                color = Color.WHITE;
+                migratingDirection = Direction.SOUTH;
+                break;
+
+            case SPRING:
+                color = Color.BROWN;
+                break;
+
+            case SUMMER:
+                migratingDirection = Direction.NORTH;
+                break;
+        }
     }
 }
